51730a826762b9040a4cc889f83353daf26ba4e6
Kyrylo Alieksieiev <alieksieiev@172.23.73.29.wi-fi.pjwstk.edu.pl>
2025-03-25 10:54:31 +0100

    Add file-based initialization for entries
    Implemented `FileService` to load entries from a CSV file during database initialization if no entries exist. Updated `DatabaseInitializator` to incorporate this functionality and added configuration support via `values.yaml`. Also included a sample `dictionary.csv` file for demonstration.

--- src/main/java/tpo/language/flashcards/FlashcardsApplication.java
+++ src/main/java/tpo/language/flashcards/FlashcardsApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import tpo.language.flashcards.controller.FlashcardsController;
+import tpo.language.flashcards.service.DatabaseInitializator;
 
 @SpringBootApplication
 public class FlashcardsApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(FlashcardsApplication.class, args);
-        FlashcardsController controller = context.getBean(FlashcardsController.class);
-        controller.start();
+        context.getBean(DatabaseInitializator.class).init();
+        context.getBean(FlashcardsController.class).start();
 
 
     }
--- src/main/java/tpo/language/flashcards/service/DatabaseInitializator.java
+++ src/main/java/tpo/language/flashcards/service/DatabaseInitializator.java
@@ -6,15 +6,17 @@ import tpo.language.flashcards.model.Entry;
 @Component
 public class DatabaseInitializator {
 
+    private FileService fileService;
     private EntryService entryService;
 
-    public DatabaseInitializator(EntryService entryService) {
+    public DatabaseInitializator(FileService fileService, EntryService entryService) {
+        this.fileService = fileService;
         this.entryService = entryService;
     }
 
     public void init() {
         if (entryService.countEntries() == 0){
-
+            fileService.loadEntries();
         }
     }
 }
